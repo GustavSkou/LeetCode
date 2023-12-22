3e3acf418e8d9adee17569061b69c010100e7a0e
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2023-12-22 23:00:41 +0100

    3. LongestSubstringWithoutRepeatingCharacters
    hashtable

--- 3. LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3. LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,35 +1,52 @@
 #include <string>
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        string currentSubstring;
+        unordered_map<char, int> substringHashMap;
+        int substringLength = 0;
         int longestSubstring = 0;
 
         for(int index = 0; index < s.size(); ++index)
         {
-            if (currentSubstring.find(s[index]) != string::npos)
-            {
-                if(currentSubstring.find(s[index]) != 0)
-                {
-                    currentSubstring.erase(0, currentSubstring.find(s[index]) + 1);
-                }
-                else
-                {
-                    currentSubstring.erase(0, 1);
-                }
-            }
-
-            currentSubstring = currentSubstring + s[index];
+            char currentLetter = s[index];
             
-            if(currentSubstring.size() > longestSubstring)
+                if (substringHashMap.find(currentLetter) != substringHashMap.end())
+                {
+                    if(substringHashMap.at(currentLetter) != 0)
+                    {
+                        substringHashMap.erase(substringHashMap.begin(), substringHashMap.find(currentLetter));
+                        substringLength = substringLength - substringHashMap.at(s[index]);
+                        //currentSubstring.erase(0, currentSubstring.find(s[index]) + 1);
+                    }
+                    else
+                    {
+                        substringHashMap.erase(substringHashMap.begin());
+                        --substringLength;
+                    }
+                }
+            
+            
+            substringHashMap.insert({s[index], substringLength});
+            ++substringLength;
+
+            if(substringLength > longestSubstring)
             {
-                longestSubstring = currentSubstring.size();
+                longestSubstring = substringLength;
             }
         }
         return longestSubstring;
     }
 };
+
+int main(){
+    string s = "abcabcds";
+
+    Solution someString;
+    cout << someString.lengthOfLongestSubstring(s);
+    return 0;
+}
