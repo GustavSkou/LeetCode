d79f0a1a45f48d8e31918b7144b46e146c558c46
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2023-12-25 19:19:07 +0100

    Update 11. ContainerWithMostWater.cpp

--- 11. ContainerWithMostWater.cpp
+++ 11. ContainerWithMostWater.cpp
@@ -7,13 +7,13 @@ class Solution {
       public:     
       int maxArea(vector<int>& height) 
       {
-            vector<int> maxContainerIndex;
             int left = 0, right = height.size()-1, maxContainerSize = 0, containerSize;
 
             for(int i = 0; i < height.size()-1; ++i)
             {
                   containerSize = (right - left) * min(height[left], height[right]);
-                  
+                  maxContainerSize = max(maxContainerSize, containerSize);
+
                   if (height[left] >= height[right])
                   {
                         --right;
@@ -22,17 +22,9 @@ class Solution {
                   {
                         ++left;
                   }
-
-                  maxContainerSize = max(maxContainerSize, containerSize);
             }
+
             return maxContainerSize;
       }
 };
 
-int main(int argc, char const *argv[])
-{
-      vector<int> height = {4,4,2,11,0,11,5,11,13,8};
-      Solution testcase;
-      cout << testcase.maxArea(height);
-      return 0;
-}
